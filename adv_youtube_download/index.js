6e5df5138de3efaa6b2a6c7cea7a760bdfca51f3
AstalNeker <85320199+NekerSqu4w@users.noreply.github.com>
2023-04-30 04:02:15 +0200

    fixed some bug

--- adv_youtube_download/index.js
+++ adv_youtube_download/index.js
@@ -18,62 +18,76 @@ dl_and_convert_audio = (videoUrl,settings) => {
     return new Promise(async (resolve, reject) => {
         if(!videoUrl.startsWith("https://www.youtube.com/watch?v=")) {reject(videoUrl + " is not valid");};
         //convert youtube info and youtube audio to data
+        
         console.log(video_identifier + "Searching video information..");
         const info = await ytdl.getInfo(videoUrl).catch(err => reject);
-        console.log(video_identifier + "Downloading video audio..");
-        const audioStream = await ytdl(videoUrl,{quality: 'highestaudio',filter: 'audioonly'});
-        const filename = info.videoDetails.title;
 
-        //write audio file
-        console.info(video_identifier + "Writing audio file as '" + (settings.path + filename + temp_id) + "'..");
-        var audioData = audioStream.pipe(fs.createWriteStream(settings.path + filename + temp_id))
+        if(info.videoDetails) {
+            const filename = info.videoDetails.title.replace(/"/g,"_").replace(/'/g,"_");
 
-        dl_detail.videoDetails = info.videoDetails;
-        dl_detail.savedAs = settings.path + filename + to_id
+            console.log(video_identifier + "Downloading video audio..");
+            const audioStream = await ytdl(videoUrl,{quality: 'highestaudio',filter: 'audioonly'});
 
-        audioData.on("finish", async () => {
-            dl_detail.convertArgsArray = [
-                "ffmpeg","-y","-i",
-                '"' + path.join(settings.path,filename + temp_id) + '"',
-                "-vn","-ar 44100","-ac 2","-ab 192k",
-                '"' + path.join(settings.path,filename + "." + settings.convert_type) + '"',
-            ];
-            dl_detail.convertArgsCmd = dl_detail.convertArgsArray.join(' ');
+            //write audio file
+            console.info(video_identifier + "Writing audio file as '" + (settings.path + filename + temp_id) + "'..");
+            
+            //crash for some reason 30/04/2023, this fixed
+            const wstream = fs.createWriteStream(settings.path + filename + temp_id);
+            var audioData = audioStream.pipe(wstream);
+            //
 
-            console.log(video_identifier + "Converting file.. (Can take several seconds to end)");
-            exec(`${dl_detail.convertArgsCmd}`,async (err) => {
-                if(err) {reject(err);}
-                else{
-                    //remove temp file
-                    console.log(video_identifier + "Removing temp audio file..");
-                    fs.unlink(path.join(settings.path,filename + temp_id),() => {})
 
-                    //rewrite mp3 tag after conversion is finished
-                    if(settings.convert_type === "mp3") {
-                        console.log(video_identifier + "Adding metadata information..");
+            dl_detail.videoDetails = info.videoDetails;
+            dl_detail.savedAs = settings.path + filename + to_id
 
-                        const tags = NodeID3.read(path.join(settings.path,filename + "." + settings.convert_type));
-                        const response = await axios.get(info.videoDetails.thumbnails[info.videoDetails.thumbnails.length-1].url,{responseType: 'arraybuffer'});
-                        const jpegBuffer = await sharp(response.data, {failOnError: false}).jpeg().toBuffer();
+            audioData.on("finish", async () => {
+                //rewrite to mp3 data, because some video as other audio type
+                dl_detail.convertArgsArray = [
+                    "ffmpeg","-y","-i",
+                    '"' + path.join(settings.path,filename + temp_id) + '"',
+                    "-vn","-ar 44100","-ac 2","-ab 192k",
+                    '"' + path.join(settings.path,filename + "." + settings.convert_type) + '"',
+                ];
+                dl_detail.convertArgsCmd = dl_detail.convertArgsArray.join(' ');
 
-                        tags.title = dl_detail.videoDetails.title
-                        tags.artist = dl_detail.videoDetails.author.name
-                        tags.image = {
-                            mime: "image/jpeg",
-                            description: "Back Cover",
-                            imageBuffer: jpegBuffer
+                console.log(video_identifier + "Converting file.. (Can take several seconds to end)");
+                exec(`${dl_detail.convertArgsCmd}`,async (err) => {
+                    if(err) {reject(err);}
+                    else{
+                        //remove temp file
+                        console.log(video_identifier + "Removing temp audio file..");
+                        fs.unlink(path.join(settings.path,filename + temp_id),() => {})
+
+                        //rewrite mp3 tag after conversion is finished
+                        if(settings.convert_type === "mp3") {
+                            console.log(video_identifier + "Adding metadata information..");
+
+                            const tags = NodeID3.read(path.join(settings.path,filename + "." + settings.convert_type));
+                            const response = await axios.get(info.videoDetails.thumbnails[info.videoDetails.thumbnails.length-1].url,{responseType: 'arraybuffer'});
+                            const jpegBuffer = await sharp(response.data, {failOnError: false}).jpeg().toBuffer();
+
+                            tags.title = dl_detail.videoDetails.title
+                            tags.artist = dl_detail.videoDetails.author.name
+                            tags.image = {
+                                mime: "image/jpeg",
+                                description: "Back Cover",
+                                imageBuffer: jpegBuffer
+                            }
+                            NodeID3.update(tags, path.join(settings.path,filename + to_id));
+
+                            //fs.writeFile("tags.txt",JSON.stringify(tags,"",4),() => {})
+                            dl_detail.metadata = tags
                         }
-                        NodeID3.update(tags, path.join(settings.path,filename + to_id));
 
-                        //fs.writeFile("tags.txt",JSON.stringify(tags,"",4),() => {})
-                        dl_detail.metadata = tags
+                        console.log(video_identifier + "Download finished with success !");
+                        resolve(dl_detail);
                     }
-
-                    console.log(video_identifier + "Download finished with success !");
-                    resolve(dl_detail);
-                }
+                });
             });
-        });
+        }
+        else{
+            reject(videoUrl + " no video details found..");
+        }
     })
 }
 
